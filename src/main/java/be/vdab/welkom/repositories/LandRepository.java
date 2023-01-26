340b6d9ac0abfa97d9474d00613df04fdb05a363
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-26 14:21:12 +0100

    hoofdstuk 4 - Bean

---
+++ src/main/java/be/vdab/welkom/repositories/LandRepository.java
@@ -0,0 +1,9 @@
+package be.vdab.welkom.repositories;
+
+import org.springframework.stereotype.Component;
+@Component
+public class LandRepository {
+    public LandRepository() {
+        System.out.println("LandRepository constructor");
+    }
+}
