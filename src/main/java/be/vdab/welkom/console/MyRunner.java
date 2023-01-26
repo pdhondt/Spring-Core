6e7eddb92038ea8ee775bd0852ff5f45882f5653
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-26 18:02:48 +0100

    hoofdstuk 5 - CommandLineRunner

---
+++ src/main/java/be/vdab/welkom/console/MyRunner.java
@@ -0,0 +1,12 @@
+package be.vdab.welkom.console;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRunner implements CommandLineRunner {
+    @Override
+    public void run (String... args) {
+        System.out.println("Hallo");
+    }
+}
