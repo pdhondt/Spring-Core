5a824806bcab174c7a5f49b2a84914b53aaca5a1
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-26 18:06:27 +0100

    hoofdstuk 5 - CommandLineRunner

--- src/main/java/be/vdab/welkom/console/MyRunner.java
+++ src/main/java/be/vdab/welkom/console/MyRunner.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyRunner implements CommandLineRunner {
     @Override
-    public void run (String... args) {
+    public void run(String... args) {
         System.out.println("Hallo");
     }
 }
