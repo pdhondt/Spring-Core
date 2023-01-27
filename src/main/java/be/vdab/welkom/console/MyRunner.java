50f9539227427771990af023ec75cb9520d97a61
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-27 15:00:47 +0100

    hoofdstuk 7 - Dependency Injection & IOC

--- src/main/java/be/vdab/welkom/console/MyRunner.java
+++ src/main/java/be/vdab/welkom/console/MyRunner.java
@@ -1,12 +1,23 @@
 package be.vdab.welkom.console;
 
+import be.vdab.welkom.exceptions.RepositoryException;
+import be.vdab.welkom.repositories.LandRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
+    private final LandRepository landRepository;
+    public MyRunner(LandRepository landRepository) {
+        this.landRepository = landRepository;
+    }
     @Override
     public void run(String... args) {
-        System.out.println("Hallo");
+        try {
+            landRepository.findAll().forEach(land ->
+                    System.out.println(land.getCode() + ":" + land.getNaam() + " " + land.getOppervlakte()));
+        } catch (RepositoryException ex) {
+            ex.printStackTrace(System.err);
+        }
     }
 }
