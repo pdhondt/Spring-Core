97d7bf7d24ac4938a4d7b8e6016db5fcfa62c84b
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-27 16:48:19 +0100

    hoofdstuk 8 - Meerdere Dependencies

--- src/main/java/be/vdab/welkom/console/MyRunner.java
+++ src/main/java/be/vdab/welkom/console/MyRunner.java
@@ -2,20 +2,25 @@ package be.vdab.welkom.console;
 
 import be.vdab.welkom.exceptions.RepositoryException;
 import be.vdab.welkom.repositories.LandRepository;
+import be.vdab.welkom.repositories.TaalRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
     private final LandRepository landRepository;
-    public MyRunner(LandRepository landRepository) {
+    private final TaalRepository taalRepository;
+    public MyRunner(LandRepository landRepository, TaalRepository taalRepository) {
         this.landRepository = landRepository;
+        this.taalRepository = taalRepository;
     }
     @Override
     public void run(String... args) {
         try {
             landRepository.findAll().forEach(land ->
-                    System.out.println(land.getCode() + ":" + land.getNaam() + " " + land.getOppervlakte()));
+                    System.out.println(land.getCode() + ": " + land.getNaam() + " " + land.getOppervlakte()));
+            taalRepository.findAll().forEach(taal ->
+                    System.out.println(taal.getCode() + ": " + taal.getNaam()));
         } catch (RepositoryException ex) {
             ex.printStackTrace(System.err);
         }
